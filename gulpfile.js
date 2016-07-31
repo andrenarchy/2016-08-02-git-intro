8e374b15ec6228570fa6a920f678b236211da419
André Gaul <andre@gaul.io>
2016-07-31 19:24:36 +0200

    use pug and sass

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var gulp = require('gulp');
+
+gulp.task('static', [], function() {
+  return gulp.src(['src/img/**/*'], {base: './src'}).pipe(gulp.dest('build'));
+});
+
+gulp.task('vendor', [], function() {
+  return gulp.src([
+    'bower_components/bootstrap/dist/**/*',
+    'bower_components/fontawesome/**/*',
+    'bower_components/roboto-fontface/**/*',
+    'bower_components/angular/angular.min.js',
+    'bower_components/angular-animate/angular-animate.min.js',
+    'bower_components/angular-highlightjs/angular-highlightjs.min.*',
+    'bower_components/angular-slidedeck/angular-slidedeck.min.*',
+  ], {base: './bower_components'})
+    .pipe(gulp.dest('build/vendor'));
+});
--- index.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-var app = angular.module('JspmAngular2', ['ngSlidedeck', 'hljs']);
